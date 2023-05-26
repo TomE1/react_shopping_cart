2e6a06215c60d094db8179597315f778758a83f0
TomE <tommevans210@gmail.com>
2023-05-26 15:00:09 +0700

    products added

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div className='cart'>
+      <div>
+        <h1>Tom's cart</h1>
+      </div>
+    </div>
+  );
+};
+export default Cart;
